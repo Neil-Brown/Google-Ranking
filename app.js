7e08e9e8af623dfd93050386871ad22ec234f6a0
Neil-Brown <46882259+Neil-Brown@users.noreply.github.com>
2020-09-06 14:11:49 +0900

    Fixed CSP for analytics
    Fixed CSP for analytics

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const bodyParser = require("body-parser");
 const compression = require('compression');
+const crypto = require('crypto');
 const express = require("express")
 const helmet = require("helmet")
 const request = require("request");
 var router = require("./routes");
 
 const environment = process.env.NODE_ENV || 3000;
+var nonce = crypto.randomBytes(16).toString('base64');
 
 // function redirect(req, res, next){
 // 	if(!process.env.PORT){
@@ -24,28 +26,38 @@ const environment = process.env.NODE_ENV || 3000;
   // App
 const app = express()
 app.use(compression({threshold:0}))
+// template variables
+app.use(function (req, res, next) {
+  app.locals.nonce = nonce
+  next();
+});
 app.use(helmet())
 app.use(helmet.referrerPolicy({ policy: 'same-origin' }))
 app.use(helmet.contentSecurityPolicy({
   directives: {
     formAction:["'self'"],
-		defaultSrc:["'self'"],
+		defaultSrc:["'none'"],
 		frameAncestors:["'none'"],
 		baseUri:["'self'"],
     objectSrc:["'none'"],
     imgSrc: [
       "'self'",
-      'data:'
+      'data:',
+      "www.google-analytics.com",
     ],
     styleSrc: [
       "'self'",
       "'sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z'", //Bootstrap CSS NOT WORKING
-      "https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+      "https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css",
     ],
     scriptSrc:[
       "'self'",
-      "'sha384-kvQSltvHvSg/3tzsKo6WscGmhSm10Tqg7f4Sn+okA4GdpCI/h8UjPpU/M4ea/z9V'" //Bootstrap native
+      `'nonce-${nonce}'`,
+      "https://www.google-analytics.com",
+      "'sha384-kvQSltvHvSg/3tzsKo6WscGmhSm10Tqg7f4Sn+okA4GdpCI/h8UjPpU/M4ea/z9V'", //Bootstrap native
+      "'sha256-ACZs81gqsgh5iAo1PHojKYR9CfZfnzwnrPyBLJ6Giok='"  // Google Analytics Inline
     ],
+    connectSrc:["https://www.google-analytics.com", "'self'"],
   }
 }))
 
