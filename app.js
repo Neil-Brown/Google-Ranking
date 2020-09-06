25ced82b6348503016659aac090886b59bc6243c
Neil-Brown <46882259+Neil-Brown@users.noreply.github.com>
2020-09-06 12:34:34 +0900

    Update app.js

--- app.js
+++ app.js
@@ -5,21 +5,21 @@ const helmet = require("helmet")
 const request = require("request");
 var router = require("./routes");
 
-const port = process.env.NODE_ENV || 3000;
+const environment = process.env.NODE_ENV || 3000;
 
-function redirect(req, res, next){
-	if(!process.env.PORT){
-	  return next();
-	}
-	let host = req.headers.host;
-
-	if (req.headers["x-forwarded-proto"] !== "https"){
-		return res.redirect(301, "https://" + req.hostname + req.url);
-	} else if(!host.match(/^www\..*/i)){
-		return res.redirect(301, "https://www." + host + req.url);
-	}
-	next();
-  };
+// function redirect(req, res, next){
+// 	if(!process.env.PORT){
+// 	  return next();
+// 	}
+// 	let host = req.headers.host;
+//
+// 	if (req.headers["x-forwarded-proto"] !== "https"){
+// 		return res.redirect(301, "https://" + req.hostname + req.url);
+// 	} else if(!host.match(/^www\..*/i)){
+// 		return res.redirect(301, "https://www." + host + req.url);
+// 	}
+// 	next();
+//   };
 
   // App
 const app = express()
@@ -49,14 +49,14 @@ app.use(helmet.contentSecurityPolicy({
   }
 }))
 
-app.all("*", redirect);
+// app.all("*", redirect);
 app.use(express.static(__dirname + "/public"));
 app.set("view engine", "pug");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(router)
-var server = app.listen(port, () => {
-   console.log(`Google Rankings listening at ` + port)
+var server = app.listen(process.env.PORT || 3000, () => {
+   console.log(`Google Rankings listening at ` + environment)
 })
 
 module.exports = server
